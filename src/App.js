36c22569e63979979464fd08ac2c9abe8e252d8c
Aanchalydv <3107aanchal@gmail.com>
2024-01-29 13:42:37 +0530

    add nested routing and css to enhance the code

--- src/App.js
+++ src/App.js
@@ -1,34 +1,30 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes,Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './Home';
 import NewPage from './NewPage';
 import Filter from './Filter';
 import Clickme from './Clickme';
+import './App.css'; 
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-      <Link to="/NewPage">About</Link>
-      <br></br>
+    <Router>
+      <div className="navbar">
       <Link to='/'>Home</Link>
-      <br></br>
-      <Link to="/Filter">Filter</Link>
-        <Routes>
-          <Route path='/' element={<Home />} >
+        <Link to="/NewPage">About</Link>
+        
+        <Link to="/Filter">Filter</Link>
+      </div>
+      <Routes>
+      <Route path='/' element={<Home />} >
           <Route path='/Clickme' element={<Clickme />} />
           </Route>
-          <Route path='/NewPage' element={<NewPage />} />
-          <Route path='/Filter' element={<Filter />} />
-        </Routes>
-      </BrowserRouter>
-  
-     
-    
-
-    </>
+        
+        <Route path='/NewPage' element={<NewPage />}></Route>
+        <Route path='/Filter' element={<Filter />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
-
--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,21 @@
-import React from 'react'
-import {Link, Outlet} from 'react-router-dom'
-
+import React from 'react';
+import { Link, Outlet, useNavigate } from 'react-router-dom';
+import './Home.css'; // Import your CSS file for styling
 
 function Home() {
+  const navigate = useNavigate();
   return (
-    <div>
-    <h1> Home page</h1>
+    <div className="home-container">
+      <h1>Home page</h1>
 
-    <Link to='/Clickme'> click</Link> 
-    <Outlet />
-    
-     </div>
+      <Link to='/Clickme'>Click</Link>
+      <Outlet /><br />
+      <button onClick={() => navigate('/Filter')}>Go to filter page</button><br />
+      <button onClick={() => navigate('/NewPage')}>Go to About page</button>
+
+    </div>
   );
 }
 
 export default Home;
+
